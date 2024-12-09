8a51d69b17dfb8691c767b389268def1aad335fa
nelsonaravena2024 <nelsonaravena2024@gmail.com>
2024-12-09 18:19:18 +0000

    feat: se creo el componente

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+function Main(){
+
+    return (
+        <div id="container">
+        <div class="buttons">
+          <input type="file" id="fileInput" />
+          <button id="actionButton">Hacer Acción</button>
+        </div>
+        <div class="divider"></div>
+        <h2>Imagen</h2>
+      </div>
+    
+    )
+}
+
+export default Main;
